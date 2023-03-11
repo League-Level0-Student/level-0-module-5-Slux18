d7cee3788040f1d788cf7e9c7a7b0fd99e09152b
league <league@imac-20.attlocal.net>
2023-03-10 16:51:48 -0800

    i started module 5

--- src/_01_algorithms/_3_goofy_names/GoofyNames.java
+++ src/_01_algorithms/_3_goofy_names/GoofyNames.java
@@ -11,29 +11,37 @@ public class GoofyNames {
 		String goofyName = "";
 
 		// 1. Ask the user to enter their name
-
+String name = JOptionPane.showInputDialog("what is your name?");
 		// 2. Print upper case name to the console using .toUpperCase()
 		//    Run your program to see that this works.
-
+System.out.println(name.toUpperCase());
 		// 3. Loop through each character of the name (steps 4 - 7).
 		//    HINT: Use .length() to determine the number of characters in the String.
 
 				// 4. Create a char variable to store the next character of the name
 				//    use .charAt()
-	
+		for(int i=0;i<name.length();i++) {
+			name.charAt(i);
+			char w= name.charAt(i);
 				
 				// 5. Use MODULO operator (%) to identify if it is an EVEN or ODD character.
-			
-			
+		if(i % 2 == 0) {
+			System.out.println(Character.toUpperCase(w));
+			 goofyName += Character.toUpperCase(w);
+		}
+		else{
+			System.out.println(Character.toLowerCase(w));
+			 goofyName += Character.toLowerCase(w);
+		}
 				// 6. Even characters should be made uppercase and odd characters made lowercase
 				//    HINT: use Character.toUpperCase() or Character.toLowerCase()
-	
+		
 			
 				// 7. ADD the char to the end of the goofyName String
 
-		
+		}
 		// 8. Use pop-up to show user their Goofy name
-
+JOptionPane.showMessageDialog(null, goofyName);
 	}
 }
 
--- src/_02_nested_loops/_1_snowflake/Snowflake.java
+++ src/_02_nested_loops/_1_snowflake/Snowflake.java
@@ -1,17 +1,24 @@
 package _02_nested_loops._1_snowflake;
 
+import java.awt.AWTException;
 import java.awt.Color;
+import java.awt.Robot;
 
 
 public class Snowflake {
 	
 	public static void main(String[] args) {
-
+		Robot rob;
 		// #1. Make a new Robot
-
+		try {
+			 rob= new Robot();
+		} catch (AWTException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 		// #2. Set your robot’s position to x=300 and y=300
-
+		
 
 		// #3. Put the robot's pen down
 

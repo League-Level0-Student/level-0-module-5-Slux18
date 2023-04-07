3a6a1483dcaf3d94e683ba323798e40529925ea0
league <alishahussain1229@gmail.com>
2023-04-07 16:57:53 -0700

    w

--- src/_03_method_writing/_1_obedient_robot/ObedientRobot.java
+++ src/_03_method_writing/_1_obedient_robot/ObedientRobot.java
@@ -5,14 +5,14 @@ import java.security.PublicKey;
 import org.jointheleague.graphical.robot.Robot;
 
 public class ObedientRobot {
-	Robot rob = new Robot();	
+static	Robot rob = new Robot();	
 public static void main(String[] args) {
 	
 drawSquare();
 drawTriangle();
 }
 public static void drawSquare() {
-	Robot rob = new Robot();	
+	
 	rob.setSpeed(255);
 	rob.penDown();
 	rob.turn(90);
@@ -31,10 +31,16 @@ public static void drawTriangle() {
 
 	rob.setSpeed(255);
 	rob.penDown();
-	for(int i=0;i<3;i++) {
-		rob.turn(-90);
-		rob.move(200);
-		rob.move(-45);
-	}
+	rob.turn(-115);
+	rob.move(220);
+	rob.turn(-127);
+	rob.move(220);
+	rob.turn(-115);
+	rob.move(200);
+	
+}
+public static void drawCircle() {
+	rob.penDown();
+
 }
 }

1b2a299fc644fbaba003560b5f0d5c6c122d10ff
league <alishahussain1229@gmail.com>
2023-04-10 18:32:56 -0700

    i did robot

--- src/_03_method_writing/_1_obedient_robot/ObedientRobot.java
+++ src/_03_method_writing/_1_obedient_robot/ObedientRobot.java
@@ -1,20 +1,57 @@
 package _03_method_writing._1_obedient_robot;
 
+import java.awt.Color;
 import java.security.PublicKey;
+import java.util.Iterator;
+
+import javax.swing.JOptionPane;
 
 import org.jointheleague.graphical.robot.Robot;
 
 public class ObedientRobot {
-static	Robot rob = new Robot();	
+static	Robot rob = new Robot();
+
 public static void main(String[] args) {
-	
-drawSquare();
+	rob.miniaturize();
+rob.penDown();
+rob.setSpeed(255);
+
+
+String favColor = JOptionPane.showInputDialog("what color do you want your shape to be?(choose colors in the rainbow or your gay)");
+if(favColor.equals("blue")) {
+	rob.setPenColor(Color.blue);
+}
+if(favColor.equals("red")) {
+	rob.setPenColor(Color.red);
+}
+if(favColor.equals("green")) {
+	rob.setPenColor(Color.green);
+}
+if(favColor.equals("purple")) {
+	rob.setPenColor(160, 32, 240);
+}
+if(favColor.equals("yellow")) {
+	rob.setPenColor(Color.yellow);
+}
+if(favColor.equals("orange")) {
+	rob.setPenColor(Color.orange);
+
+
+}
+String userResponse=JOptionPane.showInputDialog("what shape should rob draw??");
+if(userResponse.equals("circle")){
+	drawCircle();
+}
+if(userResponse.equals("triangle")) {
 drawTriangle();
+}
+if(userResponse.equals("square")) {
+drawSquare();
+}
 }
 public static void drawSquare() {
 	
-	rob.setSpeed(255);
-	rob.penDown();
+
 	rob.turn(90);
 	rob.move(200);
 	rob.turn(-90);
@@ -29,9 +66,8 @@ public static void drawSquare() {
 }
 public static void drawTriangle() {
 
-	rob.setSpeed(255);
-	rob.penDown();
-	rob.turn(-115);
+	
+
 	rob.move(220);
 	rob.turn(-127);
 	rob.move(220);
@@ -40,7 +76,14 @@ public static void drawTriangle() {
 	
 }
 public static void drawCircle() {
-	rob.penDown();
-
+	
+	
+	
+	
+	for(int i =0;i<360;i++) {
+		rob.penDown();
+		rob.turn(1);
+		rob.move(2);
+	}
 }
 }

7016ec978efb5a851df0e7726cf95ff654fa10c1
league <league@imac-20.attlocal.net>
2023-07-14 16:53:35 -0700

    .

--- src/_04_practice/_2_code_4_life/Code4Life.java
+++ src/_04_practice/_2_code_4_life/Code4Life.java
@@ -1,6 +1,5 @@
 package _04_practice._2_code_4_life;
 
-
 import java.io.File;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
@@ -8,63 +7,66 @@ import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
 public class Code4Life {
-	/***********  SOUND ***************
-	 * Some computers are unable to play sounds. 
-	 * If you cannot play sound on this computer, set canPlaySounds to false.
-	 * If you are not sure, ask your teacher 
-	 * *****************/
+	/***********
+	 * SOUND *************** Some computers are unable to play sounds. If you cannot
+	 * play sound on this computer, set canPlaySounds to false. If you are not sure,
+	 * ask your teacher
+	 *****************/
 	static boolean canPlaySounds = true;
-	private void mian() {
+
+	public static  void main(String[] args) {
+
 		// TODO Auto-generated method stub
 
-	
-	
-	// 1. Ask the user how many hours they spent coding this week.
-String userHours = JOptionPane.showInputDialog("how many hours have you spent coding this week?");
-	// 2. If it is less than or equal to 2, tell them to stop watching YouTube and
-	// write code instead.
-int time = Integer.parseInt(userHours);
-if(time <= 2) {
-	JOptionPane.showMessageDialog(null, "stop watching youtube and write code insted");
-}
-	// 3. If it is greater than or equal to 3 AND less than or equal to 5, tell them
-	// they're a Code Ninja
+		// 1. Ask the user how many hours they spent coding this week.
+		String userHours = JOptionPane.showInputDialog("how many hours have you spent coding this week?");
+		// 2. If it is less than or equal to 2, tell them to stop watching YouTube and
+		// write code instead.
+		int time = Integer.parseInt(userHours);
+		if (time <= 2) {
+			JOptionPane.showMessageDialog(null, "stop watching youtube and write code insted");
+		}
+		// 3. If it is greater than or equal to 3 AND less than or equal to 5, tell them
+		// they're a Code Ninja
+		if (time > 2 && time < 5) {
+			JOptionPane.showMessageDialog(null, "you are a cocde ninja");
+		}
+		// 4. If it is more than 5, call the playBatmanTheme method, then
+		// call the displayBatman method.
+		if (time >= 5) {
+			playBatmanTheme();
+			displayBatman();
+		}
+	}
 
-	// 4. If it is more than 5, call the playBatmanTheme  method, then 
-	//    call the displayBatman method.
-
-
-
-	
 	private static void playBatmanTheme() {
-	// NOTE: Download batman.wav from league-sounds on GitHub: https://github.com/jointheleague/league-sounds
-	//	     Then put batman.wav into the "_2_code_4_life" project
-	//       If you want to play a different audio, change the file name in the code below.
-		if (canPlaySounds) {		
+		// NOTE: Download batman.wav from league-sounds on GitHub:
+		// https://github.com/jointheleague/league-sounds
+		// Then put batman.wav into the "_2_code_4_life" project
+		// If you want to play a different audio, change the file name in the code
+		// below.
+		if (canPlaySounds) {
 			File sound = new File("src/_04_practice/_2_code_4_life/batman.wav");
 			if (sound.exists()) {
 				new Thread(() -> {
-				try {
-					Clip clip = AudioSystem.getClip();
-					clip.open(AudioSystem.getAudioInputStream(sound));
-					clip.start();
-					Thread.sleep(clip.getMicrosecondLength()/1000);
-				}
-				catch (Exception e) {
-					System.out.println("Could not play this sound");
-				}}).start();
-	 		}
-			else {
+					try {
+						Clip clip = AudioSystem.getClip();
+						clip.open(AudioSystem.getAudioInputStream(sound));
+						clip.start();
+						Thread.sleep(clip.getMicrosecondLength() / 1000);
+					} catch (Exception e) {
+						System.out.println("Could not play this sound");
+					}
+				}).start();
+			} else {
 				System.out.println("File does not exist");
 			}
 		}
 	}
-	
-	private static void displayBatman() {		
+
+	private static void displayBatman() {
 		ImageIcon icon = new ImageIcon("src/_04_practice/_2_code_4_life/batman.png");
 		JOptionPane.showMessageDialog(null, "", "You must be Batman!", 0, icon);
 	}
-	
 
-}
 }
